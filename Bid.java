23652f19219dc841f5000644536ef24d06be6972
fati1905 <fatimabenkadourr@gmail.com>
2022-12-27 00:07:20 +0100

    setter, getter, constructor and basic methods

--- Bid.java
+++ Bid.java
@@ -1,2 +1,61 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class Bid {
+    Seller seller; //The seller of the bid
+    ArrayList <Offer> offres = new ArrayList<>();
+
+    Double priceStart; //Starting price of the product
+    //todo: See how to schedule the bid later
+    Calendar calendar;
+    double timer;
+
+    public Bid(Seller seller, ArrayList<Offer> offres, Double priceStart, Calendar calendar, double timer) {
+        //todo : Add scanners
+        this.seller = seller;
+        this.offres = offres;
+        this.priceStart = priceStart;
+        this.calendar = calendar;
+        this.timer = timer;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public ArrayList<Offer> getOffres() {
+        return offres;
+    }
+
+    public Double getPriceStart() {
+        return priceStart;
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    public double getTimer() {
+        return timer;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    public void setOffres(ArrayList<Offer> offres) {
+        this.offres = offres;
+    }
+
+    public void setPriceStart(Double priceStart) {
+        this.priceStart = priceStart;
+    }
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    public void setTimer(double timer) {
+        this.timer = timer;
+    }
 }
--- Offer.java
+++ Offer.java
@@ -1,2 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Offer {
+    private double prixProp; //Le prix proposé
+    private Buyer buyer;
+
+    public Offer(Buyer buyer) {
+        this.buyer = buyer;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your price offer : ");
+        while (true){
+           try {
+               this.prixProp = sc.nextDouble();
+           }catch (InputMismatchException ex){
+               System.out.println("Input is invalid : "+ex.getMessage());
+               continue;
+           }
+           break;
+        }
+    }
+
+    public double getPrixProp() {
+        return prixProp;
+    }
+
+    public Buyer getBuyer() {
+        return buyer;
+    }
+
+    public void setPrixProp(double prixProp) {
+        this.prixProp = prixProp;
+    }
+
+    public void setBuyer(Buyer buyer) {
+        this.buyer = buyer;
+    }
 }
